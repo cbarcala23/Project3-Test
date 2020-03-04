5e775ebd9364191f0e4c7158d340114dca84992e
itadmin <itadmin@computer.hsd1.wa.comcast.net>
2020-03-03 21:08:41 -0800

    added Map component and fixes needed for Axios. Tested in Dev working

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Map from "./components/Map";
 import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
@@ -43,6 +44,7 @@ const App = () => {
               <PrivateRoute exact path="/profiles" component={Profiles} />
               <PrivateRoute exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/map" component={Map} />
               <PrivateRoute
                 exact
                 path="/create-profile"
